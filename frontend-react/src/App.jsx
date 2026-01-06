dee72e61a209dabd0df2404ed8411e25b75f01ee
Anish <121345816+itsmeanish13@users.noreply.github.com>
2026-01-06 19:11:16 +0545

    Components created

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './assets/css/style.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Body from './components/Body'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      
+      <Body />
+      <Footer />
+      
       
     </>
   )
